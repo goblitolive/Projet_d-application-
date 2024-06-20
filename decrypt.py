cb6c88229416c411a9a24cfddaa1fa7c04464e72
Mehdi <mehdikasmi125@gmail.com>
2024-06-20 21:36:17 +0200

    .

--- decrypt.py
+++ decrypt.py
@@ -149,3 +149,25 @@ def generate_possible_mappings(ciphertext, known_frequency, known_words, known_b
         adjusted_mappings.append(adjusted_mapping)
 
     return adjusted_mappings
+from difflib import SequenceMatcher
+
+def find_most_similar_known_words(cipher_word, known_words, top_n=4):
+    """
+    Cette fonction prend un mot chiffré, calcule sa longueur, et cherche dans known_words les mots
+    de même longueur en les classant par ordre de similarité décroissante.
+
+    :param cipher_word: Le mot chiffré.
+    :param known_words: Une liste de mots connus.
+    :param top_n: Le nombre de mots les plus similaires à retourner (par défaut 4).
+    :return: Une liste des top_n mots connus les plus similaires.
+    """
+    cipher_word_length = len(cipher_word)
+    same_length_words = [word for word in known_words if len(word) == cipher_word_length]
+    
+    def similarity(a, b):
+        return SequenceMatcher(None, a, b).ratio()
+    
+    scored_words = [(word, similarity(cipher_word, word)) for word in same_length_words]
+    scored_words.sort(key=lambda item: item[1], reverse=True)
+    
+    return [word for word, score in scored_words[:top_n]]
--- essai.py
+++ essai.py
@@ -1,17 +1,28 @@
-def chiffrer_avec_cle(text, key):
-    encrypted_text = []
-    for char in text:
-        if char in key:
-            encrypted_text.append(key[char])
-        else:
-            encrypted_text.append(char)
-    return ''.join(encrypted_text)
+from difflib import SequenceMatcher
 
-# Clé de substitution
-update = {'t': 'i','i':'s','.':'q','s':'t'}
+def find_most_similar_known_words(cipher_word, known_words, top_n=4):
+    """
+    Cette fonction prend un mot chiffré, calcule sa longueur, et cherche dans known_words les mots
+    de même longueur en les classant par ordre de similarité décroissante.
 
-# Texte à chiffrer
-text ='en mq cgarlei francoti btenvenu myrtel esats eve.ue de dthneqc esats un vtetllard d envtron iotxanse.utnze ani tl occupats le itehede dthne deputi q.uot.ue ce desatl ne soucge en aucune mantere au fond meme de ce .uenoui avoni a raconser tl n eis peusesre pai tnustle ne fusce .uepour esre exacs en sous d tndt.uer tct lei brutsi es lei propoi .utavatens couru iur ion compse au momens ou tl esats arrtve dani ledtoceieq vrat ou faux ce .u on dts dei gommei stens iouvens ausans deplace dani leur vte es iursous dani leur deistnee .ue ce .u tli fonsq mqmyrtel esats ftli d un conietller au parlemens d atx nobleiie de robeqon consats de lut .ue ion pere le reiervans pour gertser de ia cgarhe l avats marte de fors bonne geure a dtxguts ou vtnhs ani iutvans unuiahe aiiez repandu dani lei famtllei parlemensatreiq cgarlei myrtel nonobisans ce martahe avats dtiatson beaucoup fats parler de lutq tlesats bten fats de ia perionne .uot.ue d aiiez pestse satlle elehans hracteux iptrtsuel souse la premtere parste de ia vte avats ese donneeau monde es aux halanserteiq la revoluston iurvtns lei evenemensi ieprectptserens lei famtllei parlemensatrei dectmeei cgaiieei sra.ueei ie dtiperierensq mq cgarlei myrtel dei lei premteri jouri de larevoluston emthra en tsalteq ia femme y mourus d une maladte depotsrtne dons elle esats assetnse deputi lonhsempiq tli n avatens potnsd enfansiq .ue ie paiiastl'
-# Chiffrement du texte
-encrypted_text = chiffrer_avec_cle(text, update)
-print(encrypted_text)
+    :param cipher_word: Le mot chiffré.
+    :param known_words: Une liste de mots connus.
+    :param top_n: Le nombre de mots les plus similaires à retourner (par défaut 4).
+    :return: Une liste des top_n mots connus les plus similaires.
+    """
+    cipher_word_length = len(cipher_word)
+    same_length_words = [word for word in known_words if len(word) == cipher_word_length]
+    
+    def similarity(a, b):
+        return SequenceMatcher(None, a, b).ratio()
+    
+    scored_words = [(word, similarity(cipher_word, word)) for word in same_length_words]
+    scored_words.sort(key=lambda item: item[1], reverse=True)
+    
+    return [word for word, score in scored_words[:top_n]]
+
+# Exemple d'utilisation
+cipher_word = "exemple"
+known_words = ["exemple", "sample", "temple", "example", "simple","eaample","exenple,exmlpe"]
+similar_words = find_most_similar_known_words(cipher_word, known_words)
+print(similar_words)
